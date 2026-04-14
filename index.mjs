883df79af1da2a2a53a934ea61479ccb2d0493ad
baibhavtibrewal <baibhavtibrewal9@gmail.com>
2026-04-15 01:48:56 +0530

    fix: use railway db variables

--- index.mjs
+++ index.mjs
@@ -46,14 +46,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = process.env.PORT || 8080;
 
 const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  max: process.env.DB_MAX,
-  connectionTimeoutMillis: 0,
-  idleTimeoutMillis: 0,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const app = express();
